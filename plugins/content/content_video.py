6d5e587e31b76162502b634fe1a80ba07c0ba12c
n4sty001 <46842988+n4sty001@users.noreply.github.com>
2019-01-19 22:33:10 +0300

    Update content_video.py

--- plugins/content/content_video.py
+++ plugins/content/content_video.py
@@ -8,7 +8,7 @@ class VideoPlugin(CommandPlugin):
         """Plugin sending some videos by request."""
 
         if not commands:
-            commands = ("видео", "видос")
+            commands = ("видео", "видос", "бан")
 
         super().__init__(*commands, prefixes=prefixes, strict=strict)
 
@@ -20,7 +20,7 @@ class VideoPlugin(CommandPlugin):
             q=self.parse_message(msg, full=True)[1] or "милый котик",
             sort=2,
             count=10,
-            adult=0,
+            adult=1,
             offset= random.randint(1, 300)
         )
 
