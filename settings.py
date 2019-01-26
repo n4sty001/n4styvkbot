82c1729392f6c0d91710de6aacbbd3883ffc571d
n4sty001 <46842988+n4sty001@users.noreply.github.com>
2019-01-26 22:11:45 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ from settings_base import BaseSettings  # Importing base settings
 class BotSettings(BaseSettings):
     # See README.md for details!
     USERS = (    
-         ("user", "adab1f98c4f7dac0fb9d0eba3bd156cfc2d49d252494a5b47bb268b40dd303b45cadd16e02fe0d04610f5",),
+         ("user", "7c326a5e8fb63ab992ef373a21cae8a16deb5b32d413c2da290f19bb246e180f2207c04f12fc63a664154",),
     )
 
     # Default settings for plugins
